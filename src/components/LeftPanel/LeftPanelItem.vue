fdeee7d5922bda92349a12f07bb7c4377f006be8
Vasiliy Smirnov <vasya.smirnoff24@yandex.ru>
2020-12-04 15:43:58 +0300

    Part of templates added

---
+++ src/components/LeftPanel/LeftPanelItem.vue
@@ -0,0 +1,13 @@
+<template>
+
+</template>
+
+<script>
+export default {
+name: "LeftPanelItem"
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/components/TopPanel/User.vue
@@ -0,0 +1,13 @@
+<template>
+
+</template>
+
+<script>
+export default {
+name: "USer"
+}
+</script>
+
+<style scoped>
+
+</style>
